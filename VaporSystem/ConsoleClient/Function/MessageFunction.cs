136da66911cc410c2c4a26433dd7ee114e96255b
German Konopka <konopkagerman@gmail.com>
2021-09-11 18:47:36 -0300

    fixup! Add IFunction and MessageFunction

---
+++ VaporSystem/ConsoleClient/Function/MessageFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using FunctionInterface;
+using Protocol;
+using SocketLogic;
+
+namespace ConsoleClient.Function
+{
+    public class MessageFunction : IFunction
+    {
+        public void Execute(Socket socket, Header header)
+        {
+            Console.WriteLine("Ingrese el mensaje:");
+            var message = Console.ReadLine();
+            var newHeader = new Header(HeaderConstants.Request, FunctionConstants.Message, message.Length);
+            SocketManager.Send(socket, newHeader, message);
+        }
+    }
+}
