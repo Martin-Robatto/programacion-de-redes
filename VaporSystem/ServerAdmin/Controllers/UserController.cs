b4b9090cd6c48b7d2fed90ea5c4b252e9cdaa67b
Martin Robatto Otegui <martinrobatto8@gmail.com>
2021-11-15 08:53:45 -0300

    WebApi created

---
+++ VaporSystem/ServerAdmin/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerAdmin.Models;
+
+namespace ServerAdmin.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] UserModelIn model)
+        {
+            return Ok(null);
+        }
+        
+        [HttpDelete]
+        public IActionResult Delete([FromRoute] string username)
+        {
+            return Ok(null);
+        }
+        
+        [HttpPut]
+        public IActionResult Delete([FromRoute] string username, [FromBody] UserModelIn model)
+        {
+            return Ok(null);
+        }
+    }
+}
---
+++ VaporSystem/ServerAdmin/Models/UserModelIn.cs
@@ -0,0 +1,8 @@
+﻿namespace ServerAdmin.Models
+{
+    public class UserModelIn
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
